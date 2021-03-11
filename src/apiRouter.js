d7567e6fa00ab0bc23c0972d7518528fadee853c
Camille Maurel <camille.maurel@epitech.eu>
2021-03-11 15:32:52 +0100

    API REST: User can remove songs from a playlists

--- src/apiRouter.js
+++ src/apiRouter.js
@@ -81,6 +81,38 @@ router.post('/songs/add_to_playlist', (req, res) => {
     }
 })
 
+router.post('/songs/remove_from_playlist', (req, res) => {
+    const headers = req.headers
+    const data = req.body
+    const [type, token] = headers.authorization.split(' ')
+
+    if (type != "Bearer") {
+        res.status(400).json({ "error": "bad_type" })
+        console.log("API: Bad type send")
+    } else {
+        if (!data.idSong || !data.idPlaylist) {
+            res.status(400).json({ "error": "bad_arguments" })
+            console.log("API: bad_arguments")
+        } else {
+            jwt.verify(token, "VerySecretKeyAccessToken", function (error, tokenContent) {
+                if (error) {
+                    res.status(400).json(error)
+                } else {
+                    db.removeSongToPlaylist(data.idPlaylist, data.idSong, function (error) {
+                        if (error) {
+                            res.status(400).json(error)
+                            console.log(error)
+                        } else {
+                            res.status(201).json({ "success": true })
+                            console.log("API: Remove song from a playlist")
+                        }
+                    })
+                }
+            })
+        }
+    }
+})
+
 router.get('/playlists', (req, res) => {
     const headers = req.headers
     const [type, token] = headers.authorization.split(' ')

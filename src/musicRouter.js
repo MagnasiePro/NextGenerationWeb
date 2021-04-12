84b626a5888689587c6c6cd3cf9e1fc65606f50a
Camille Maurel <camille.maurel@epitech.eu>
2021-04-12 16:20:35 +0200

    Connect Songs to RDF

--- src/musicRouter.js
+++ src/musicRouter.js
@@ -1,19 +1,47 @@
 const express = require('express')
 const db = require('./db')
+const { EnapsoGraphDBClient } = require('@innotrade/enapso-graphdb-client');
+
+// connection data to the running GraphDB instance
+const GRAPHDB_BASE_URL = 'http://localhost:7200',
+    GRAPHDB_REPOSITORY = 'Postify',
+    GRAPHDB_USERNAME = 'server',
+    GRAPHDB_PASSWORD = 'test',
+
+    DEFAULT_PREFIXES = [
+        EnapsoGraphDBClient.PREFIX_OWL,
+        EnapsoGraphDBClient.PREFIX_RDF,
+        EnapsoGraphDBClient.PREFIX_RDFS,
+        EnapsoGraphDBClient.PREFIX_XSD,
+        EnapsoGraphDBClient.PREFIX_PROTONS,
+        {
+            prefix: 'ont',
+            iri: "https://dbpedia.org/ontology/"
+        },
+        EnapsoGraphDBClient.PREFIX_PROTONS,
+        {
+            prefix: 'sch',
+            iri: "https://schema.org/"
+        }
+    ];
+
+let graphDBEndpoint = new EnapsoGraphDBClient.Endpoint({
+    baseURL: GRAPHDB_BASE_URL,
+    repository: GRAPHDB_REPOSITORY,
+    prefixes: DEFAULT_PREFIXES,
+    transform: 'toCSV'
+});
+
+graphDBEndpoint
+    .login(GRAPHDB_USERNAME, GRAPHDB_PASSWORD)
+    .then((result) => {
+        console.log(result);
+    })
+    .catch((err) => {
+        console.log(err);
+    });
 
 const router = express.Router()
-
-router.post('/add-song', (req, res) => {
-    const data = req.body
-
-    if (!data.name || !data.artist) {
-        console.log("ERROR: Missing arguments")
-    } else {
-        db.addSong(data.name, data.artist, function (error, id) {
-            res.redirect("/")
-        })
-    }
-})
 
 router.post('/add-song-to-playlist', (req, res) => {
     const data = req.body
@@ -47,15 +75,31 @@ router.post('/remove-song-to-playlist', (req, res) => {
 })
 
 router.get('/', (req, res) => {
-    db.getSongs(function (error, songs) {
-        db.getPlaylistsByOwnerId(req.session.userID, function (error, playlists) {
-            const model = {
-                songs: songs,
-                playlists: playlists
-            }
-            res.render('songs.hbs', model)
+    graphDBEndpoint
+        .query(
+            `select *
+        where {
+            ?class a ont:Song ;
+                sch:name ?name ;
+                sch:byArtist ?artist ;
+                sch:duration ?duration ;
+                sch:inAlbum ?album ;
+                sch:thumbnailUrl ?thumbnailUrl .
+        }`,
+            { transform: 'toJSON' }
+        )
+        .then((result) => {
+            db.getPlaylistsByOwnerId(req.session.userID, function (error, playlists) {
+                const model = {
+                    songs: result.records,
+                    playlists: playlists
+                }
+                res.render('songs.hbs', model)
+            })
         })
-    })
+    .catch((err) => {
+        console.log(err);
+    });
 })
 
 module.exports = router
